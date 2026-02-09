c4625d0cbacf9160ff88f3a845b38970563a7568
toxo-42 <kimuh.0402@gmail.com>
2026-02-09 11:06:28 +0900

    PeOb PatchNote - 목소리 성별 설정
    기존 여성 목소리에서 앱 안에 Butten으로 male, female 전환 시도 해봤으니 삼성 제조사 커스터마이징이 강력하므로 fail
    
    대신 삼성 음성설정 바로가기 기능 voice setting popup에 추가

--- app/src/main/java/com/example/agproject/BleService.kt
+++ app/src/main/java/com/example/agproject/BleService.kt
@@ -303,7 +303,7 @@ class BleService : Service(), TextToSpeech.OnInitListener {
     // 3. 말하기
     tts?.speak(text, TextToSpeech.QUEUE_FLUSH, null, "ID")
   }
-  
+
   private fun startForegroundServiceNotification(content: String) {
     val notification = createNotification(content)
     if (Build.VERSION.SDK_INT >= 34) {
